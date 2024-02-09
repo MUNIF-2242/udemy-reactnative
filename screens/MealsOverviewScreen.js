1b7d57d1cc8b1adcf7f99f824a4e7ccc627fdd0d
MUNIF <munif@shellbeehaken.com>
2024-02-09 14:47:23 +0600

    display meals in meals overview screen

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { MEALS } from "../data/data";
+import MealItem from "../components/MealItem";
 
 const MealsOverviewScreen = ({ route }) => {
   const catId = route.params.categoryId;
+
+  const disPlayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+  const renderMealItem = (itemData) => {
+    return <MealItem title={itemData.item.title} />;
+  };
   return (
     <View>
-      <Text>MealsOverviewScreen -{catId}</Text>
+      <Text>
+        <FlatList
+          data={disPlayedMeals}
+          keyExtractor={(item) => {
+            return item.id;
+          }}
+          renderItem={renderMealItem}
+        />
+      </Text>
     </View>
   );
 };
