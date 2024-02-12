f0d8ba676480158aa03c10ddaf0c59bdce04d96b
MUNIF <munif@shellbeehaken.com>
2024-02-12 15:30:40 +0600

    Adding, Configuring & Using Bottom Tabs

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import UserScreen from "./screens/UserScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
-  const Drawer = createDrawerNavigator();
+  const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="WelcomeScreen" component={WelcomeScreen} />
-        <Drawer.Screen name="UserScreen" component={UserScreen} />
-      </Drawer.Navigator>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "#eb1064",
+        }}
+      >
+        <Tab.Screen
+          name="WelcomeScreen"
+          component={WelcomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="UserScreen"
+          component={UserScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
