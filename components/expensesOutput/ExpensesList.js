bd59b862615f1be70f7baa349d57fb88d7877cd4
MUNIF <munif@shellbeehaken.com>
2024-02-14 14:31:57 +0600

    Continuing Work on the Expense-related Components

---
+++ components/expensesOutput/ExpensesList.js
@@ -0,0 +1,14 @@
+import { FlatList, StyleSheet, View } from "react-native";
+import React from "react";
+
+const ExpensesList = () => {
+  return (
+    <View>
+      <FlatList data={{}} renderItem={{}} />
+    </View>
+  );
+};
+
+export default ExpensesList;
+
+const styles = StyleSheet.create({});
